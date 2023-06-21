35930810daec58ca906dc140bb8fc47fd6960ce9
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-06-20 19:09:28 -0700

    Added register view component

---
+++ src/components/register-view/registerview.jsx
@@ -0,0 +1,65 @@
+export const RegisterView = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        const data = {
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        }
+        fetch("https://ba-movie-api.herokuapp.com/user/register",{
+        Method: "POST", body: JSON.stringify(data), headers: {"Content-Type": "application/json"}}).then((response)=>{
+            if(response.ok) {
+                alert("Registered successfully!");
+                window.location.reload();
+            } else {
+                alert("Registration failed!");
+            }
+        });
+    };
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username: <input
+                type="text"
+                value={username}
+                onChange={(e)=>setUsername(e.target.value)}
+                required
+                />
+            </label>
+            <br/>
+            <label>
+                Password: <input
+                type="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required
+                />
+            </label>
+            <br/>
+            <label>
+                Email: <input
+                type="email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+                />
+            </label>
+            <br/>
+            <label>
+                Birthday: <input
+                type="date"
+                value={birthday}
+                onChange={(e)=>setBirthday(e.target.value)}
+                required
+                />
+            </label>
+            <br/>
+        </form>
+    );
+}
