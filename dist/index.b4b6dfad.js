c71f4b795a83be8afea53cb2bfb0309754e0cdcd
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-08 17:11:52 -0700

    Profileview done

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -47406,22 +47406,18 @@ var _react = require("react");
 var _s = $RefreshSig$();
 const ProfileUpdateView = ({ user })=>{
     _s();
-    let oldPassword = "";
-    let confirmed = false;
     const navigate = (0, _reactRouter.useNavigate)();
     const [newPassword, setNewPassword] = (0, _react.useState)("");
-    const [currentPassword, setCurrentPassword] = (0, _react.useState)("");
     const [newEmail, setNewEmail] = (0, _react.useState)("");
     const updateInfo = (event)=>{
         event.preventDefault();
-        const data = {
-            Username: newUsername,
-            Password: newPassword,
-            Email: newEmail
-        };
-        fetch("https://ba-movie-api.herokuapp.com/user/update", {
+        let info = {};
+        if (newPassword) info["Password"] = newPassword;
+        if (newEmail) info["Email"] = newEmail;
+        if (Object.keys(info).length === 0) alert("No info to update!");
+        else fetch("https://ba-movie-api.herokuapp.com/user/update", {
             Method: "PATCH",
-            body: JSON.stringify(data),
+            body: JSON.stringify(info),
             headers: {
                 "Content-Type": "application/json"
             }
@@ -47431,145 +47427,107 @@ const ProfileUpdateView = ({ user })=>{
             navigate("/profile");
         });
     };
-    function finalize() {}
-    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-        children: !confirmed ? /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Col), {
-            md: 3,
-            className: "mt-5rem font-white",
-            children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form), {
-                onSubmit: finalize,
-                children: [
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
-                        children: [
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Label, {
-                                children: "New Password:"
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 45,
-                                columnNumber: 29
-                            }, undefined),
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Control, {
-                                type: "password",
-                                value: newPassword,
-                                onChange: (e)=>setNewPassword(e.target.value)
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 46,
-                                columnNumber: 29
-                            }, undefined)
-                        ]
-                    }, void 0, true, {
-                        fileName: "src/components/profile-view/profile-update-view.jsx",
-                        lineNumber: 44,
-                        columnNumber: 25
-                    }, undefined),
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
-                        children: [
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Label, {
-                                children: "New Email:"
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 53,
-                                columnNumber: 29
-                            }, undefined),
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Control, {
-                                type: "email",
-                                value: newEmail,
-                                onChange: (e)=>setNewEmail(e.target.value)
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 54,
-                                columnNumber: 29
-                            }, undefined)
-                        ]
-                    }, void 0, true, {
-                        fileName: "src/components/profile-view/profile-update-view.jsx",
-                        lineNumber: 52,
-                        columnNumber: 25
-                    }, undefined),
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
-                        inline: true,
-                        className: "mt-1rem",
-                        children: [
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
-                                className: "",
-                                type: "submit",
-                                children: "Update"
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 61,
-                                columnNumber: 29
-                            }, undefined),
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("span", {
-                                children: " OR: "
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 62,
-                                columnNumber: 29
-                            }, undefined),
-                            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
-                                href: "/profile",
-                                children: "Cancel"
-                            }, void 0, false, {
-                                fileName: "src/components/profile-view/profile-update-view.jsx",
-                                lineNumber: 63,
-                                columnNumber: 29
-                            }, undefined)
-                        ]
-                    }, void 0, true, {
-                        fileName: "src/components/profile-view/profile-update-view.jsx",
-                        lineNumber: 60,
-                        columnNumber: 25
-                    }, undefined)
-                ]
-            }, void 0, true, {
-                fileName: "src/components/profile-view/profile-update-view.jsx",
-                lineNumber: 43,
-                columnNumber: 21
-            }, undefined)
-        }, void 0, false, {
+    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Col), {
+        md: 3,
+        className: "mt-5rem font-white",
+        children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form), {
+            onSubmit: updateInfo,
+            children: [
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
+                    children: [
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Label, {
+                            children: "New Password:"
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 42,
+                            columnNumber: 21
+                        }, undefined),
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Control, {
+                            type: "password",
+                            value: newPassword,
+                            onChange: (e)=>setNewPassword(e.target.value)
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 43,
+                            columnNumber: 21
+                        }, undefined)
+                    ]
+                }, void 0, true, {
+                    fileName: "src/components/profile-view/profile-update-view.jsx",
+                    lineNumber: 41,
+                    columnNumber: 17
+                }, undefined),
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
+                    children: [
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Label, {
+                            children: "New Email:"
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 50,
+                            columnNumber: 21
+                        }, undefined),
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Control, {
+                            type: "email",
+                            value: newEmail,
+                            onChange: (e)=>setNewEmail(e.target.value)
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 51,
+                            columnNumber: 21
+                        }, undefined)
+                    ]
+                }, void 0, true, {
+                    fileName: "src/components/profile-view/profile-update-view.jsx",
+                    lineNumber: 49,
+                    columnNumber: 17
+                }, undefined),
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Group, {
+                    inline: true,
+                    className: "mt-1rem",
+                    children: [
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
+                            className: "",
+                            type: "submit",
+                            children: "Update"
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 58,
+                            columnNumber: 21
+                        }, undefined),
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("span", {
+                            children: " OR: "
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 59,
+                            columnNumber: 21
+                        }, undefined),
+                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
+                            href: "/profile",
+                            children: "Cancel"
+                        }, void 0, false, {
+                            fileName: "src/components/profile-view/profile-update-view.jsx",
+                            lineNumber: 60,
+                            columnNumber: 21
+                        }, undefined)
+                    ]
+                }, void 0, true, {
+                    fileName: "src/components/profile-view/profile-update-view.jsx",
+                    lineNumber: 57,
+                    columnNumber: 17
+                }, undefined)
+            ]
+        }, void 0, true, {
             fileName: "src/components/profile-view/profile-update-view.jsx",
-            lineNumber: 42,
-            columnNumber: 17
-        }, undefined) : /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Col), {
-            md: 3,
-            children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form), {
-                onSubmit: updateInfo,
-                children: [
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Label, {
-                        children: "Enter Current Password: "
-                    }, void 0, false, {
-                        fileName: "src/components/profile-view/profile-update-view.jsx",
-                        lineNumber: 70,
-                        columnNumber: 25
-                    }, undefined),
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Form).Control, {
-                        type: "password",
-                        value: currentPassword,
-                        onChange: (e)=>setCurrentPassword(e.target.value)
-                    }, void 0, false, {
-                        fileName: "src/components/profile-view/profile-update-view.jsx",
-                        lineNumber: 71,
-                        columnNumber: 25
-                    }, undefined)
-                ]
-            }, void 0, true, {
-                fileName: "src/components/profile-view/profile-update-view.jsx",
-                lineNumber: 69,
-                columnNumber: 21
-            }, undefined)
-        }, void 0, false, {
-            fileName: "src/components/profile-view/profile-update-view.jsx",
-            lineNumber: 68,
-            columnNumber: 17
+            lineNumber: 40,
+            columnNumber: 13
         }, undefined)
     }, void 0, false, {
         fileName: "src/components/profile-view/profile-update-view.jsx",
-        lineNumber: 40,
+        lineNumber: 39,
         columnNumber: 9
     }, undefined);
 };
-_s(ProfileUpdateView, "m/xTjoJ3+9+S/rr3t27bktJfwi8=", false, function() {
+_s(ProfileUpdateView, "vIt1fx/YZPppItr+3NLfFRp4FxM=", false, function() {
     return [
         (0, _reactRouter.useNavigate)
     ];
@@ -47583,6 +47541,6 @@ $RefreshReg$(_c, "ProfileUpdateView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react-bootstrap":"3AD9A","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","react-router":"dbWyW"}],"lJZlQ":[function() {},{}]},["4s3Ar","1xC6H","d8Dch"], "d8Dch", "parcelRequire315a")
+},{"react/jsx-dev-runtime":"iTorj","react-bootstrap":"3AD9A","react-router":"dbWyW","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"lJZlQ":[function() {},{}]},["4s3Ar","1xC6H","d8Dch"], "d8Dch", "parcelRequire315a")
 
 //# sourceMappingURL=index.b4b6dfad.js.map
--- src/components/profile-view/profile-update-view.jsx
+++ src/components/profile-view/profile-update-view.jsx
@@ -3,81 +3,63 @@ import { useNavigate } from "react-router";
 import { useState } from "react";
 
 export const ProfileUpdateView = ({user}) => {
-    let oldPassword = "";
-    let confirmed = false;
     const navigate = useNavigate();
     const [newPassword, setNewPassword] = useState("");
-    const [currentPassword, setCurrentPassword] = useState("");
     const [newEmail, setNewEmail] = useState("");
 
     const updateInfo = (event) => {
         event.preventDefault();
         
-        const data = {
-            Username: newUsername,
-            Password: newPassword,
-            Email: newEmail
+        let info = {}
+        if(newPassword) {
+            info["Password"] = newPassword;
         }
-        
-        fetch("https://ba-movie-api.herokuapp.com/user/update",{
-        Method: "PATCH", 
-        body: JSON.stringify(data), 
-        headers: {"Content-Type": "application/json"}}).then((response)=>{
-            if(response.ok) {
-                alert("Updated successfully!");
-            } else {
-                alert("Update failed!");
-            }
-            navigate("/profile");
-        });
+        if(newEmail)
+        {
+            info["Email"] = newEmail;
+        }
+        if(Object.keys(info).length === 0) { 
+            alert("No info to update!");
+        } else {
+            fetch("https://ba-movie-api.herokuapp.com/user/update",{
+            Method: "PATCH", 
+            body: JSON.stringify(info), 
+            headers: {"Content-Type": "application/json"}}).then((response)=>{
+                if(response.ok) {
+                    alert("Updated successfully!");
+                } else {
+                    alert("Update failed!");
+                }
+                navigate("/profile");
+            });
+        }
     }
-    function finalize() {
-        
-    }
-
 
     return (
-        <div>
-            {!confirmed ? (
-                <Col md={3} className="mt-5rem font-white">
-                    <Form onSubmit={finalize}>
-                        <Form.Group>
-                            <Form.Label>New Password:</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={newPassword}
-                                onChange={(e)=>setNewPassword(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>New Email:</Form.Label>
-                            <Form.Control
-                                type="email"
-                                value={newEmail}
-                                onChange={(e)=>setNewEmail(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group inline className="mt-1rem">
-                            <Button className="" type="submit">Update</Button>
-                            <span> OR: </span>
-                            <Button href="/profile">Cancel</Button>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            ) : (
-                <Col md={3}>
-                    <Form onSubmit={updateInfo}>
-                        <Form.Label>Enter Current Password: </Form.Label>
-                        <Form.Control
-                            type="password"
-                            value={currentPassword}
-                            onChange={(e)=>setCurrentPassword(e.target.value)}
-                        />
-                    </Form>
-                </Col>
-            )}
-            
-        </div>
-        
+        <Col md={3} className="mt-5rem font-white">
+            <Form onSubmit={updateInfo}>
+                <Form.Group>
+                    <Form.Label>New Password:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={newPassword}
+                        onChange={(e)=>setNewPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>New Email:</Form.Label>
+                    <Form.Control
+                        type="email"
+                        value={newEmail}
+                        onChange={(e)=>setNewEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group inline className="mt-1rem">
+                    <Button className="" type="submit">Update</Button>
+                    <span> OR: </span>
+                    <Button href="/profile">Cancel</Button>
+                </Form.Group>
+            </Form>
+        </Col>
     );
 }
