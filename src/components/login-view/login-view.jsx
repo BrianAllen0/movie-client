e9c032f214f602da1815bd2a58bbfc79ea936a58
schnecke <eiweala@live.com>
2023-07-14 08:51:41 -0700

    Initial setup

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,59 +1,62 @@
 import { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
+import axios from "axios";
 
-export const LoginView = ({onLoggedIn, onClickRegister}) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = {
-            access: username,
-            secret: password
-        };
-        fetch(`https://ba-movie-api.herokuapp.com/user/login?Username=${data.access}&Password=${data.secret}`,
-        {method: "POST", headers: {"Content-Type": "application/json"}}).then((response)=>response.json()).then((data)=>{
-            console.log(data);
-            if(data.user) {
-                localStorage.setItem("user", JSON.stringify(data.user));
-                localStorage.setItem("token", JSON.stringify(data.token));
-                onLoggedIn(data.user, data.token);
-            } else {
-                alert("Invalid Credentials!");
-            }
-        }).catch((e)=>{
-            alert("Something went wrong!");
-        });
+export const LoginView = ({ onLoggedIn, onClickRegister }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      Username: username,
+      Password: password,
     };
-    const changeToRegister = () =>{
-        onClickRegister();
-    };
-    return(
-        <Col md={3} className="mt-5rem">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={username}
-                        onChange={(e)=>setUsername(e.target.value)}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="formPassword">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e)=>setPassword(e.target.value)}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group inline className="mt-1rem">
-                    <Button type="submit">Login</Button>
-                    <span> OR: </span>
-                    <Button onClick={changeToRegister}>Register</Button>
-                </Form.Group>
-            </Form>
-        </Col>
-    );
-}
+    axios
+      .post(`https://ba-movie-api.herokuapp.com/user/login`, data)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.user) {
+          localStorage.setItem("user", JSON.stringify(response.data.user));
+          localStorage.setItem("token", response.data.token);
+          onLoggedIn(response.data.user, response.data.token);
+        } else {
+          alert("Invalid Credentials!");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong!");
+      });
+  };
+  const changeToRegister = () => {
+    onClickRegister();
+  };
+  return (
+    <Col md={3} className="mt-5rem">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group inline className="mt-1rem">
+          <Button type="submit">Login</Button>
+          <span> OR: </span>
+          <Button onClick={changeToRegister}>Register</Button>
+        </Form.Group>
+      </Form>
+    </Col>
+  );
+};
--- src/components/profile-view/profile-update-view.jsx
+++ src/components/profile-view/profile-update-view.jsx
@@ -1,65 +1,72 @@
 import { Col, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useState } from "react";
+import axios from "axios";
 
-export const ProfileUpdateView = ({user}) => {
-    const navigate = useNavigate();
-    const [newPassword, setNewPassword] = useState("");
-    const [newEmail, setNewEmail] = useState("");
+export const ProfileUpdateView = ({ user }) => {
+  const navigate = useNavigate();
+  const [newPassword, setNewPassword] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
-    const updateInfo = (event) => {
-        event.preventDefault();
-        
-        let info = {}
-        if(newPassword) {
-            info["Password"] = newPassword;
-        }
-        if(newEmail)
-        {
-            info["Email"] = newEmail;
-        }
-        if(Object.keys(info).length === 0) { 
-            alert("No info to update!");
-        } else {
-            fetch("https://ba-movie-api.herokuapp.com/user/update",{
-            Method: "PATCH", 
-            body: JSON.stringify(info), 
-            headers: {"Content-Type": "application/json"}}).then((response)=>{
-                if(response.ok) {
-                    alert("Updated successfully!");
-                } else {
-                    alert("Update failed!");
-                }
-                navigate("/profile");
-            });
-        }
+  console.log(user);
+
+  const updateInfo = (event) => {
+    event.preventDefault();
+
+    let info = {
+      Username: user.Username,
+    };
+    if (newPassword.trim().length > 0) {
+      info.Password = newPassword;
+    }
+    if (newEmail.trim().length > 0 && newEmail.includes("@")) {
+      info.Email = newEmail;
+    }
+    if (!info.Password && !info.Email) {
+      alert("No info to update!");
+      return;
     }
 
-    return (
-        <Col md={3} className="mt-5rem font-white">
-            <Form onSubmit={updateInfo}>
-                <Form.Group>
-                    <Form.Label>New Password:</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={newPassword}
-                        onChange={(e)=>setNewPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>New Email:</Form.Label>
-                    <Form.Control
-                        type="email"
-                        value={newEmail}
-                        onChange={(e)=>setNewEmail(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group inline className="mt-1rem">
-                    <Button className="" type="submit">Update</Button>
-                    <span> OR: </span>
-                    <Button href="/profile">Cancel</Button>
-                </Form.Group>
-            </Form>
-        </Col>
-    );
-}
+    axios
+      .patch("https://ba-movie-api.herokuapp.com/user/update", info)
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        alert("Updated successfully!");
+        window.location = "/profile";
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("Update failed!");
+      });
+  };
+
+  return (
+    <Col md={3} className="mt-5rem font-white">
+      <Form onSubmit={updateInfo}>
+        <Form.Group>
+          <Form.Label>New Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>New Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group inline className="mt-1rem">
+          <Button className="" type="submit">
+            Update
+          </Button>
+          <span> OR: </span>
+          <Button href="/profile">Cancel</Button>
+        </Form.Group>
+      </Form>
+    </Col>
+  );
+};
