83388e94e6f48d9be3c2b788b0af1124a4513161
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-12-27 12:36:27 -0800

    Fixed birthday formatting

--- src/components/register-view/register-view.jsx
+++ src/components/register-view/register-view.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 
-export const RegisterView = ({onClickLogin}) => {
+export const RegisterView = ({ onClickLogin }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [birthday, setBirthday] = useState("");
+    const [birthday, setBirthday] = useState(new Date());
     const [email, setEmail] = useState("");
 
     const handleSubmit = (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
         const data = {
             Username: username,
             Password: password,
             Email: email,
-            Birthday: birthday
-        }
-        fetch("https://ba-movie-api.herokuapp.com/user/register",{
-        Method: "POST", 
-        body: JSON.stringify(data), 
-        headers: {"Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("token")}`}}).then((response)=>{
-            if(response.ok) {
+            Birthday: birthday,
+        };
+        fetch("https://ba-movie-api.herokuapp.com/user/register", {
+            Method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }).then((response) => {
+            if (response.ok) {
                 alert("Registered successfully!");
                 window.location.reload();
             } else {
@@ -27,54 +28,47 @@ export const RegisterView = ({onClickLogin}) => {
             }
         });
     };
-    const changeToLogin = () =>{
+    const changeToLogin = () => {
         onClickLogin();
-    }
+    };
 
-    return(
+    return (
         <Col md={3} className="mt-5rem">
             <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label>Username:</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={username}
-                        onChange={(e)=>setUsername(e.target.value)}
-                        required
-                    />
-                </Form.Group>    
+                    <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                </Form.Group>
                 <Form.Group>
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e)=>setPassword(e.target.value)}
-                        required
-                    />
+                    <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control
-                        type="email"
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                        required
-                    />
+                    <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Birthday:</Form.Label>
                     <Form.Control
+                        type="date"
                         value={birthday}
-                        onChange={(e)=>setBirthday(e.target.value)}
+                        onChange={(e) => {
+                            setBirthday(e.target.value);
+                            console.log(birthday.toDateString());
+                        }}
                         required
                     />
                 </Form.Group>
                 <Form.Group inline className="mt-1rem">
-                    <Button className="" type="submit">Register</Button>
+                    <Button className="" type="submit">
+                        Register
+                    </Button>
                     <span> OR: </span>
-                    <Button className="" onClick={changeToLogin}>Login</Button>
+                    <Button className="" onClick={changeToLogin}>
+                        Login
+                    </Button>
                 </Form.Group>
             </Form>
         </Col>
     );
-}
+};
